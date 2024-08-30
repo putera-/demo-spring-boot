32366d631132c86daf3cdc180e9eda22589d078b
Rizjami Putera <rizjamiputera@gmail.com>
2024-08-31 01:13:29 +0700

    init

---
+++ src/main/java/com/example/putera_demo/PuteraDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.putera_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuteraDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuteraDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/putera_demo/PuteraDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.putera_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuteraDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
