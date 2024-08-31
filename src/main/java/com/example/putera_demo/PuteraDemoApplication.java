77e0c6fa21293e6ad1dcc7c934a3090ba2bf6b30
Rizjami Putera <rizjamiputera@gmail.com>
2024-08-31 18:32:20 +0700

    Create User Module: get all, get by id

--- src/main/java/com/example/putera_demo/PuteraDemoApplication.java
+++ src/main/java/com/example/putera_demo/PuteraDemoApplication.java
@@ -1,13 +1,19 @@
 package com.example.putera_demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class PuteraDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PuteraDemoApplication.class, args);
+	/**
+  * The main entry point for the Putera Demo application.
+  * This method sets the default server port to 8081 and runs the Spring Boot application.
+  */
+ public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(PuteraDemoApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+		app.run(args);
 	}
-
 }
